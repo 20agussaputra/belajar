533b64950ca2f7f959eb82cf511dbc26fbcb3c1e
agus <20agussaputra@gmail.com>
2016-03-08 11:14:08 +0700

    belajar

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Online Shop &trade;</title>
+		<script type="text/javascript" src="includes/jquery.js"></script>
+		<link rel="stylesheet" href="includes/style.css"></link>
+	</head>
+		<body>
+		<div id="utama">
+		<div id="header"></div>
+		<div id="menu"></div>
+			<div id="konten">
+		<div id="kiri"></div>
+		<div id="kanan"></div>
+			</div>
+		<div id="footer"></div>
+		</div>
+		</body>
+</html>
